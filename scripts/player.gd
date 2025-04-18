74bf88a8200eb7e2a0745f2824a696559b22cd3f
Eyosias <75771087+eyos-ias@users.noreply.github.com>
2025-04-18 13:11:16 +0300

    checking owner added

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,8 +4,15 @@ const SPEED = 300.0
 
 var client_id: int
 
+func _ready() -> void:
+	GDSync.connect_gdsync_owner_changed(self, owner_changed)
+
+
 func _physics_process(delta: float) -> void:
 	# Get input direction for all four directions
+	if !GDSync.is_gdsync_owner(self):
+		return
+	
 	var direction = Vector2.ZERO
 	direction.x = Input.get_axis("ui_left", "ui_right")
 	direction.y = Input.get_axis("ui_up", "ui_down")
@@ -18,3 +25,7 @@ func _physics_process(delta: float) -> void:
 		velocity = velocity.move_toward(Vector2.ZERO, SPEED)
 	
 	move_and_slide()
+
+
+func owner_changed(new_owner: int) -> void:
+	var is_owner: bool = GDSync.is_gdsync_owner(self)
--- scripts/player_customization.gd
+++ scripts/player_customization.gd
@@ -3,12 +3,17 @@ extends Control
 @onready var host: Button = $VBoxContainer/Host
 @onready var join: Button = $VBoxContainer/Join
 @onready var creating_lobby: Label = $CreatingLobby
+@onready var vbox_container: VBoxContainer = $VBoxContainer
+@onready var game = $Game
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	game.visible = false
 	GDSync.lobby_created.connect(lobby_created)
 	GDSync.lobby_joined.connect(lobby_joined)
 	GDSync.lobby_join_failed.connect(lobby_join_failed)
-		
+	GDSync.client_joined.connect(client_joined)
+	GDSync.client_left.connect(client_left)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -29,13 +34,22 @@ func _on_join_pressed() -> void:
 func lobby_created(lobby_id: String) -> void:
 	print("Hosting lobby", " lobby id: ", lobby_id)
 	GDSync.lobby_join(lobby_id)
-	get_tree().change_scene_to_file("res://scenes/game.tscn")
+	# get_tree().change_scene_to_file("res://scenes/game.tscn")
 
 
 func lobby_joined(lobby_id: String) -> void:
 	print("Lobby joined: ", lobby_id)
-	get_tree().change_scene_to_file("res://scenes/game.tscn")
+	# get_tree().change_scene_to_file("res://scenes/game.tscn")
 	# get_tree().change_scene_to_file("res://scenes/game.tscn")
 
 func lobby_join_failed(error: int) -> void:
 	print("Lobby join failed: ", error)
+
+func client_joined(client_id: int) -> void:
+	print("Client joined: ", client_id)
+	vbox_container.visible = false
+	game.visible = true
+	
+
+func client_left(client_id: int) -> void:
+	print("Client left: ", client_id)
